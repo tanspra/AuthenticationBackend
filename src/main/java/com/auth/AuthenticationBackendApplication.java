504876afe1c9194ba097f7670f6d68ec271fc1cf
Prashant <tanspra@gmail.com>
2024-01-16 13:59:42 +0530

    Initial commit

---
+++ src/main/java/com/auth/AuthenticationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationBackendApplication.class, args);
+    }
+
+}
